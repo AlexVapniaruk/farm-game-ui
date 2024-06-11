ceb8199283e66a1a4ad95d0347730771a21a6df9
Alex Vap <alexvap23@gmail.com>
2024-06-11 16:11:59 +0300

    fix env 3

--- src/axios.ts
+++ src/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     //@ts-ignore
-    baseURL: process.env.API_BASE_URL, // Access environment variable
+    baseURL: process.env.VITE_API_BASE_URL, // Access environment variable
     timeout: 1000,
     headers: { 'X-Custom-Header': 'foobar' }
 });
