262ccf5646f14ab90f50f5c160be8431327865db
Alex Vap <alexvap23@gmail.com>
2024-06-11 19:00:07 +0300

    fix env 18

--- src/api-sdk/sockets.ts
+++ src/api-sdk/sockets.ts
@@ -1,9 +1,10 @@
 import { io } from 'socket.io-client';
 
-//@ts-ignore
-console.log(window.__env__, 'eves');
-//@ts-ignore
-const socket = io(window.__env__.API_URL || 'http://localhost:3000');
+let BASE_URL;
+//BASE_URL = import.meta.env.API_BASE_URL;
+BASE_URL = 'https://farm-game-server-ff82e5096013.herokuapp.com';
+//BASE_URL = 'http://localhost:3000';
+const socket = io(BASE_URL);
 
 // Example of setting up event listeners
 socket.on('connection', () => {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-console.log('app start');
 import { createApp } from 'vue'
-import 'vuetify/dist/vuetify.min.css'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are importing Vuetify CSS
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import * as components from 'vuetify/components'
@@ -9,47 +8,14 @@ import router from "./router.ts";
 import './style.css'
 import App from './App.vue'
 import { createVuetify } from 'vuetify'
-import axios from 'axios';
-console.log('start')
+
+const app = createApp(App)
 
 const vuetify = createVuetify({
     components,
     directives
 })
 
-console.log('/env before')
-axios.get('/env')
-    .then(response => {
-        console.log('response /env')
-        const envVariables = response.data;
-        console.log(envVariables)
-        //@ts-ignore
-        window.__env__ = envVariables; // Set environment variables on window object
-        const app = createApp(App)
-        app.use(vuetify);
-        app.use(router).mount('#app') // Initialize Vue app after environment variables are set
-    })
-    .catch(error => {
-        console.error('Error fetching environment variables:', error);
-        // Handle error if needed
-    });
+app.use(vuetify)
 
-async function initApp() {
-    try {
-        const response = await axios.get('/env');
-        const envVariables = response.data;
-        console.log('response /env');
-        console.log(envVariables);
-        //@ts-ignore
-        window.__env__ = envVariables; // Set environment variables on window object
-        const app = createApp(App)
-        app.use(vuetify);
-        app.use(router).mount('#app');
-    } catch (error) {
-        console.error('Error fetching environment variables:', error);
-        // Handle error if needed, e.g., show an error message to the user
-    }
-}
-
-// Call the function to initialize the app
-initApp();
+app.use(router).mount('#app')
