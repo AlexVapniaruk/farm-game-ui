e0e9908515bbee4cf2ae6311e75335c5279fc8c3
Alex Vap <alexvap23@gmail.com>
2024-06-11 16:12:33 +0300

    fix env 4

--- src/axios.ts
+++ src/axios.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+//@ts-ignore
+console.log(process.env.VITE_API_BASE_URL)
+//@ts-ignore
+console.log(process.env.API_BASE_URL)
 const instance = axios.create({
     //@ts-ignore
     baseURL: process.env.VITE_API_BASE_URL, // Access environment variable
