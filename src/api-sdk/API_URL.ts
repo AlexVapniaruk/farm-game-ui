31d680445827516bb4b0532821158e5829414f04
Alex Vap <alexvap23@gmail.com>
2024-06-11 13:30:12 +0300

    update types issues 2

--- src/api-sdk/API_URL.ts
+++ src/api-sdk/API_URL.ts
@@ -1,13 +1,18 @@
-interface API_URL {
-    [key: string]: string;
-}
-
 const baseUrl = import.meta.env.BASE_URL;
-// Create the object and assign the type
+
+type APIFunction<T extends any[]> = (...args: T) => string;
+
+// Define the structure of API_URL using the generic function type
+type API_URL = {
+    CREATE_ROOM: APIFunction<[playerId: string]>;
+    JOIN_ROOM: APIFunction<[roomId: string, playerId: string]>;
+    GET_ROOM: APIFunction<[roomId: string]>;
+};
+
 const apiUrls: API_URL = {
-    CREATE_ROOM: (playerId) => `${baseUrl}create-room/${playerId}`,
-    JOIN_ROOM: (roomId, playerId) => `${baseUrl}join-room/${roomId}/${playerId}`,
-    GET_ROOM: (roomId) => `${baseUrl}rooms/${roomId}`
+    CREATE_ROOM: (playerId: string) => `${baseUrl}create-room/${playerId}`,
+    JOIN_ROOM: (roomId: string, playerId: string) => `${baseUrl}join-room/${roomId}/${playerId}`,
+    GET_ROOM: (roomId: string) => `${baseUrl}rooms/${roomId}`
 };
 
 export default apiUrls;
