187be01be23b1458e5cc6681d803d18f437427b6
Alex Vap <alexvap23@gmail.com>
2024-06-11 19:00:34 +0300

    fix env 19

--- src/axios.ts
+++ src/axios.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
+let BASE_URL;
+//BASE_URL = import.meta.env.API_BASE_URL;
+BASE_URL = 'https://farm-game-server-ff82e5096013.herokuapp.com';
+//BASE_URL = 'http://localhost:3000';
 // Create Axios instance after fetching environment variables
 const instance = axios.create({
     //@ts-ignore
-    baseURL: window.__env__.API_URL, // Use the retrieved base URL
+    baseURL: BASE_URL, // Use the retrieved base URL
     timeout: 1000,
     headers: { 'X-Custom-Header': 'foobar' }
 });
