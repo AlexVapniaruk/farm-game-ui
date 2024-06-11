2e887c527296bfc813589cb0718c7a4cb27d4c22
Alex Vap <alexvap23@gmail.com>
2024-06-11 19:51:24 +0300

    fix env 21

--- server.js
+++ server.js
@@ -3,27 +3,40 @@ import serveStatic from 'serve-static';
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors'; // Import cors middleware
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const app = express()
+const app = express();
 
 const ENV_VARIABLES = {
     API_URL: process.env.VITE_API_BASE_URL || 'https://example.com/api',
     // Add more environment variables as needed
 };
 
-//here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+// CORS configuration
+const corsOptions = {
+    origin: process.env.VITE_API_BASE_URL, // Replace with your backend server URL
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+};
 
+app.use(cors(corsOptions)); // Use CORS middleware
+
+// Serve static files
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+// Endpoint to expose environment variables
 app.get('/env', (req, res) => {
     res.json(ENV_VARIABLES);
 });
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
 
-const port = process.env.PORT || 8080
-app.listen(port)
-console.log(`app is listening on port: ${port}`)
+// Route to serve the frontend application on different page routes
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log(`app is listening on port: ${port}`);
