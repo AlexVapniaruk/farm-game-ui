995af3816d38e577c221e94a332bdeb3d7791f39
Alex Vap <alexvap23@gmail.com>
2024-06-11 18:39:03 +0300

    fix env 16

--- src/api-sdk/sockets.ts
+++ src/api-sdk/sockets.ts
@@ -1,9 +1,24 @@
 import { io } from 'socket.io-client';
+import axios from "axios";
+import {createApp} from "vue";
+import App from "@/App.vue";
+import router from "@/router.ts";
 
-//@ts-ignore
-console.log(window.__env__);
-//@ts-ignore
-const socket = io(window.__env__.API_URL || 'http://localhost:3000');
+let socket;
+axios.get('/env')
+    .then(response => {
+        console.log('response /env')
+        const envVariables = response.data;
+        console.log(envVariables)
+        //@ts-ignore
+        window.__env__ = envVariables; // Set environment variables on window object
+        //@ts-ignore
+        const socket = io(window.__env__.API_URL || 'http://localhost:3000');
+    })
+    .catch(error => {
+        console.error('Error fetching environment variables:', error);
+        // Handle error if needed
+    });
 
 // Example of setting up event listeners
 socket.on('connection', () => {
